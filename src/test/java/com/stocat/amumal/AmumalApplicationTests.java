1a81f38eecd05f6bea3d63c96a3bda8c1d5a4660
xEzIxX <by1094@gachon.ac.kr>
2026-08-07 13:16:48 +0900

    test: 테스트 환경에 test 프로파일 적용

--- src/test/java/com/stocat/amumal/AmumalApplicationTests.java
+++ src/test/java/com/stocat/amumal/AmumalApplicationTests.java
@@ -2,8 +2,10 @@ package com.stocat.amumal;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class AmumalApplicationTests {
 
   @Test
